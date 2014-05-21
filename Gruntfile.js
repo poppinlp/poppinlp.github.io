2ca99924f0a0c74aeb3724560f747f9820bb15b1
PoppinL <poppinlp@gmail.com>
2014-05-21 14:27:27 +0800

    add html hint

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function(grunt) {
             }
         },
         jshint: {
-            options: grunt.file.readJSON(__dirname + '/.jshintrc'),
+            options: grunt.file.readJSON(__dirname + '/grunt/config/jshintrc'),
             www: {
                 src: ['src/js/*.js']
             }
@@ -76,33 +76,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadTasks('./grunt/task/');
 
-    grunt.registerTask('md2html', 'Build html pages from markdown file', function () {
-        var done = this.async(),
-            md = require('marked'),
-            markup = grunt.file.read('src/template.html', { encoding: 'utf8' }),
-            destPath, text;
-
-        md.setOptions({
-            renderer: new md.Renderer(),
-            gfm: true,
-            tables: true,
-            breaks: true,
-            smartLists: true
-        });
-
-        grunt.file.recurse('src/md', function (path, rootdir, subdir, file) {
-            file = file.slice(0, -3);
-            text = md(grunt.file.read(path, { encoding: 'utf8' }));
-            markup = markup.replace('{{Title}}', file);
-            markup = markup.replace('{{Content}}', text);
-            file += '.html';
-            destPath = subdir ? 'pages/' + subdir + '/' + file : 'pages/' + file;
-            grunt.file.write(destPath, markup, { encoding: 'utf8' });
-            grunt.log.ok('Build ' + destPath + ' successfully.');
-            done();
-        });
-    });
-
-    grunt.registerTask('default', ['jshint', 'uglify', 'sass', 'imagemin', 'htmlmin', 'md2html']);
+    grunt.registerTask('default', ['jshint', 'uglify', 'sass', 'imagemin', 'md2html', 'htmlmin', 'htmlhint']);
 };
---
+++ grunt/task/md2html.js
@@ -0,0 +1,28 @@
+module.exports = exports = function(grunt) {
+    grunt.registerTask('md2html', 'Build html pages from markdown file', function () {
+        var done = this.async(),
+            md = require('marked'),
+            markup = grunt.file.read('src/template.html', { encoding: 'utf8' }),
+            destPath, text;
+
+        md.setOptions({
+            renderer: new md.Renderer(),
+            gfm: true,
+            tables: true,
+            breaks: true,
+            smartLists: true
+        });
+
+        grunt.file.recurse('src/md', function (path, rootdir, subdir, file) {
+            file = file.slice(0, -3);
+            text = md(grunt.file.read(path, { encoding: 'utf8' }));
+            markup = markup.replace('{{Title}}', file);
+            markup = markup.replace('{{Content}}', text);
+            file += '.html';
+            destPath = subdir ? 'pages/' + subdir + '/' + file : 'pages/' + file;
+            grunt.file.write(destPath, markup, { encoding: 'utf8' });
+            grunt.log.ok('Build ' + destPath + ' successfully.');
+            done();
+        });
+    });
+}
