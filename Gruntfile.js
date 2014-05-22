ac6720aa8ab125c2d6254332abef96205bd49aac
PoppinL <poppinlp@gmail.com>
2014-05-22 14:41:18 +0800

    add grunt task: sprite

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,74 +1,104 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        uglify: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/js',
-                    src: '*.js',
-                    dest: 'js'
-                }]
+    var configObj = {};
+
+    // uglify
+    configObj.uglify = {
+        dist: {
+            files: [{
+                expand: true,
+                cwd: 'src/js',
+                src: '*.js',
+                dest: 'js'
+            }]
+        }
+    };
+
+    // sass
+    configObj.sass = {
+        dist: {
+            options: {
+                style: 'compressed',
+                banner: '/* Please do not change this file. It is made by sass. */'
+            },
+            files: [{
+                expand: true,
+                cwd: 'src/sass',
+                src: '*.scss',
+                dest: 'css',
+                ext: '.css'
+            }]
+        }
+    };
+
+    // imagemin
+    configObj.imagemin = {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'src/img/',
+                src: ['*.{png,jpg}'],
+                dest: 'img/'
+            }]
+        }
+    };
+
+    // htmlmin
+    configObj.htmlmin = {
+        dist: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true
+            },
+            files: {
+                'index.html': 'src/index.html'
+            }
+        }
+    };
+
+    // jshint
+    configObj.jshint = {
+        options: grunt.file.readJSON(__dirname + '/grunt/config/jshintrc'),
+        www: {
+            src: ['src/js/*.js']
+        }
+    };
+
+    // watch
+    configObj.watch = {
+        scripts: {
+            files: ['src/js/*.js'],
+            tasks: ['jshint', 'uglify'],
+            options: {
+                interrupt: true
             }
         },
         sass: {
-            dist: {
-                options: {
-                    style: 'compressed',
-                    banner: '/* Please do not change this file. It is made by sass. */'
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'src/sass',
-                    src: '*.scss',
-                    dest: 'css',
-                    ext: '.css'
-                }]
-            }
-        },
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/img/',
-                    src: ['*.{png,jpg}'],
-                    dest: 'img/'
-                }]
-            }
-        },
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    'index.html': 'src/index.html'
-                }
-            }
-        },
-        jshint: {
-            options: grunt.file.readJSON(__dirname + '/grunt/config/jshintrc'),
-            www: {
-                src: ['src/js/*.js']
-            }
-        },
-        watch: {
-            scripts: {
-                files: ['src/js/*.js'],
-                tasks: ['jshint', 'uglify'],
-                options: {
-                    interrupt: true
-                }
-            },
-            sass: {
-                files: ['src/sass/*.scss'],
-                tasks: ['sass'],
-                options: {
-                    interrupt: true
-                }
+            files: ['src/sass/*.scss'],
+            tasks: ['sass'],
+            options: {
+                interrupt: true
             }
         }
-    });
+    };
+
+    // sprite
+    var fs = require('fs'), dirList, dir, spritePath = 'src/sprite/';
+    configObj.sprite = {};
+    dirList = fs.readdirSync(spritePath);
+    while (dir = dirList.pop()) {
+        configObj.sprite[dir] = {
+            src: spritePath + dir + '/*.png',
+            destImg: 'src/img/' + dir + '.png',
+            destCSS: 'src/sass/sprite/_' + dir + '.scss',
+            imgPath: '../img/' + dir + '.png',
+            imgOpts: {
+                format: 'png',
+                quality: 100
+            }
+        }
+    }
+
+    grunt.config.init(configObj);
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
@@ -76,7 +106,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-spritesmith');
     grunt.loadTasks('./grunt/task/');
 
-    grunt.registerTask('default', ['jshint', 'uglify', 'sass', 'imagemin', 'md2html', 'htmlmin', 'htmlhint']);
+    grunt.registerTask('default', ['jshint', 'uglify', 'sprite', 'sass', 'imagemin', 'md2html', 'htmlmin', 'htmlhint']);
 };
